7f8af476568806e6e8f7522ce6ba9e8c4718446d
yangchuan <yangchuan@douyu.tv>
2018-03-12 15:50:22 +0800

    添加超时处理

--- index.js
+++ index.js
@@ -10,19 +10,28 @@ const templateRootDir = path.join(__dirname, "/../build/");
 const app = new Koa();
 const router = new Router();
 
+const maxResponseTime = 500;
+
 // 超时处理 错误处理
 app.use(async (ctx, next) => {
   const returnDefault = async function() {
     const defaultHtml = await readFile(templateRootDir + "index.html");
-    console.log(333, defaultHtml);
+    // console.log(333, defaultHtml);
     ctx.body = defaultHtml
       .replace(`{{title}}`, "鱼吧-斗鱼社区,直播爱好者的聚集地")
       .replace("{{data|safe}}", "{}");
   };
-  // let timer = setTimeout(returnDefautl, 1);
+  let timer = setTimeout(async () => {
+    // 绕过koa 标示
+    ctx.respond = false;
+
+    const defaultHtml = await returnDefault();
+    ctx.res.end(ctx.body);
+  }, maxResponseTime);
+  // console.log(444, ctx);
   try {
     await next();
-    // clearTimeout(timer);
+    clearTimeout(timer);
   } catch (e) {
     const defaultHtml = await returnDefault();
   }
@@ -32,6 +41,8 @@ app.use(async (ctx, next) => {
 const index = require("./routes/index");
 const user = require("./routes/user");
 const post = require("./routes/post");
+
+router.use("/", index.routes(), index.allowedMethods());
 
 // 匹配任意路由
 router.use("/:x", index.routes(), index.allowedMethods());
@@ -43,12 +54,8 @@ app.use(router.routes());
 app.use(
   router.allowedMethods({
     throw: true,
-    methodNotAllowed: () => {
-      console.log(3344444);
-    },
-    notImplemented: () => {
-      console.log(332233);
-    }
+    methodNotAllowed: () => {},
+    notImplemented: () => {}
   })
 );
 
--- routes/post.js
+++ routes/post.js
@@ -4,8 +4,10 @@ const axios = require("axios");
 
 router.get("/", async function(ctx, next) {
   const pid = ctx.params.pid;
+  // console.log(new Date().getTime());
   const emotionObj = await axios("https://yuba.douyu.com/wbapi/web/emotion");
   const emotionString = JSON.stringify({ emotion: emotionObj.data.emotion });
+  // console.log(new Date().getTime());
   await ctx.render("index.html", {
     title: "this is dynamic title111",
     data: emotionString
